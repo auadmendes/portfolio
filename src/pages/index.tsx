a31136ee3bd8439d4d0f84cc40676e7d144903aa
auadmendes <luciano.auad@gmail.com>
2022-12-12 12:57:40 -0300

    new deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -69,13 +69,13 @@ export default function Home({
         <ContactMe />
       </section>
 
-      {/* <Link href={'#hero'}>
+      <Link href={'#hero'}>
         <footer className="sticky bottom-5 w-full cursor-pointer">
           <div className="flex items-center justify-center">
             <Image src={hLogoImage} alt="" className="h-10 w-10 filter grayscale hover:grayscale-0 " />
           </div>
         </footer>
-      </Link> */}
+      </Link>
 
     </div>
   )
