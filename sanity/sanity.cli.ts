684ede2187b8c06a95fe09549190182f3680edbe
auadmendes <luciano.auad@gmail.com>
2022-12-12 16:43:36 -0300

    sanity 2

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -1,8 +1,8 @@
- import {defineCliConfig} from 'sanity/cli'
+ import {defineCliConfig} from '@sanity/cli'
 
-// export default defineCliConfig({
-//   api: {
-//     projectId: 'lip8p7sh',
-//     //dataset: 'test'
-//   }
-// })
+export default defineCliConfig({
+  api: {
+    projectId: 'lip8p7sh',
+    dataset: 'test'
+  }
+})
