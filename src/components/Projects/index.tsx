17f3a04df8b8f891aab4785a9684359ff701777c
auadmendes <luciano.auad@gmail.com>
2022-12-16 18:02:01 -0300

    Image

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -28,7 +28,13 @@ export function Projects({ projects }: Props) {
             className="w-screen flex-shrink-0 snap-center flex flex-col space-y-0
           items-center justify-center p-6 md:p-44 h-screen"
           >
-            <motion.img
+            <Image
+              src={project?.image.url}
+              alt={project?.title}
+              width={300}
+              height={300}
+            />
+            {/* <motion.img
               initial={{
                 y: -300,
                 opacity: 0,
@@ -38,7 +44,7 @@ export function Projects({ projects }: Props) {
               viewport={{ once: true }}
               src={project?.image.url}
               className="object-contain w-full h-96"
-            />
+            /> */}
             <div className="space-y-6 px-0 md:px-2 max-w-6xl">
               <h4 className="text-2xl lg:text-4xl font-semibold text-center">
                 <span className="underline decoration-cyan-400/20">Case Study {i + 1} of {projects.length}</span> {project.title}
